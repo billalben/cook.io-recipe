2d555525f4d481a8e23007a02379bbcf4a48ce4c
Benzazoua <billalben252@gmail.com>
2024-01-08 16:00:25 +0100

    Setup API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,31 @@
 "use strict";
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+const APP_ID = "d9170f66";
+const API_KEY = "363e4ed102e0231077d7a3e1f562155d";
+const TYPE = "public";
+
+/**
+ *
+ * @param {Array} queries Query array
+ * @param {Function} successCallback Success callback function
+ */
+
+export const fetchData = async function (queries, successCallback) {
+  const query = queries
+    ?.join("&")
+    .replace(/,/g, "=")
+    .replace(/ /g, "20%")
+    .replace(/\+/g, "2B%");
+
+  const url = `${ACCESS_POINT}?app_id=${APP_ID}&app_key=${API_KEY}&type=${TYPE}${
+    query ? `&${query}` : ""
+  }`;
+
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = response.json();
+    successCallback(data);
+  }
+};
