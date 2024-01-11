d85530371a764dcf8be622ad11c251a5e3b709fc
Benzazoua <billalben252@gmail.com>
2024-01-11 15:41:05 +0100

    snackbar :love_letter:

--- assets/js/global.js
+++ assets/js/global.js
@@ -44,11 +44,29 @@ window.saveRecipe = function (element, recipeId) {
       );
       element.classList.toggle("saved");
       element.classList.toggle("removed");
+      showNotification("Added to Recipe book");
     });
     ACCESS_POINT = ROOT;
   } else {
     window.localStorage.removeItem(`cookio-recipe${recipeId}`);
     element.classList.toggle("saved");
     element.classList.toggle("removed");
+    showNotification("Remove from Recipe book");
   }
 };
+
+const $snackbarContainer = document.createElement("div");
+$snackbarContainer.classList.add("snackbar-container");
+document.body.appendChild($snackbarContainer);
+
+function showNotification(message) {
+  const $snackbar = document.createElement("div");
+  $snackbar.classList.add("snackbar");
+  $snackbar.innerHTML = `<p class="body-medium">${message}</p>`;
+
+  $snackbarContainer.appendChild($snackbar);
+
+  $snackbar.addEventListener("animationend", () =>
+    $snackbarContainer.removeChild($snackbar)
+  );
+}
