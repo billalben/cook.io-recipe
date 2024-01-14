a83efd584f91630c6c6c8b6cbe72e59348035068
Benzazoua <billalben252@gmail.com>
2024-01-14 12:19:11 +0100

    Recipe page - render recipe

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -120,3 +120,128 @@ const $filterBtn = document.querySelector("[data-filter-btn]");
 window.addEventListener("scroll", () => {
   $filterBtn.classList[window.scrollY >= 120 ? "add" : "remove"]("active");
 });
+
+/**
+ * Request recipe and render
+ */
+
+const $gridList = document.querySelector("[data-grid-list]");
+const $loadMore = document.querySelector("[data-load-more]");
+const defaultQueries = [
+  ["mealType", "breakfast"],
+  ["mealType", "dinner"],
+  ["mealType", "lunch"],
+  ["mealType", "snack"],
+  ["mealType", "teatime"],
+  ...cardQueries,
+];
+
+$gridList.innerHTML = $skeletonCard.repeat(12);
+let nextPageUrl = "";
+
+const renderRecipe = (data) => {
+  data.hits.map((item, index) => {
+    const {
+      recipe: { image, label: title, totalTime: cookingTime, uri },
+    } = item;
+
+    const recipeId = uri.slice(uri.lastIndexOf("_") + 1);
+    const isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`);
+
+    const $card = document.createElement("div");
+    $card.classList.add("card");
+    $card.style.animationDelay = `${100 * index}ms`;
+
+    $card.innerHTML = `
+        <figure class="card-media img-holder">
+          <img
+            src="${image}"
+            width="195"
+            height="195"
+            loading="lazy"
+            alt="${title}"
+            class="img-cover"
+          />
+        </figure>
+        <div class="card-body">
+          <h3 class="title-small">
+            <a 
+              href="./detail.html?recipe=${recipeId}" 
+              class="card-link">
+              ${title ?? "Untitled"}
+            </a>
+          </h3>
+          <div class="meta-wrapper">
+            <div class="meta-item">
+              <span class="material-symbols-outlined" aria-hidden="true">schedule</span>
+              <span class="label-medium">${getTime(cookingTime).time || "<1"} 
+                ${getTime(cookingTime).timeUnit}</span>
+            </div>
+            <button class="icon-btn has-state ${
+              isSaved ? "saved" : "removed"
+            }" aria-label="Add to save recipes"
+            onclick="saveRecipe(this, '${recipeId}')">
+              <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
+              <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
+            </button>
+          </div>
+        </div>
+    `;
+
+    $gridList.appendChild($card);
+  });
+};
+
+let requestedBefore = true;
+
+fetchData(queries || defaultQueries, (data) => {
+  const {
+    _links: { next },
+  } = data;
+
+  nextPageUrl = next?.href;
+
+  $gridList.innerHTML = "";
+  requestedBefore = false;
+
+  if (data.hits.length) {
+    renderRecipe(data);
+  } else {
+    $loadMore.innerHTML = `<p class="body-medium info-text">No recipe found</p>`;
+  }
+});
+
+const CONTAINER_MAX_WIDTH = 1200;
+const CONTAINER_MAX_CARD = 6;
+
+window.addEventListener("scroll", async (e) => {
+  if (
+    $loadMore.getBoundingClientRect().top < innerHeight &&
+    !requestedBefore &&
+    nextPageUrl
+  ) {
+    $loadMore.innerHTML = $skeletonCard.repeat(
+      Math.round(
+        ($loadMore.clientWidth / CONTAINER_MAX_WIDTH) * CONTAINER_MAX_CARD
+      )
+    );
+
+    requestedBefore = true;
+
+    const response = await fetch(nextPageUrl);
+    const data = await response.json();
+
+    const {
+      _links: { next },
+    } = data;
+    nextPageUrl = next?.href;
+
+    renderRecipe(data);
+    $loadMore.innerHTML = "";
+    requestedBefore = false;
+  }
+
+  if (!nextPageUrl) {
+    $loadMore.innerHTML = `<p class="body-medium info-text">No more recipes</p>`;
+  }
+});
